a3ac61cfd91ea80c11012cab0fef4c2083059f4e
armani <razzarioa@gmail.com>
2019-02-19 22:18:49 +0600

    Added .idea to gitignore, get rid App.js and App.css redundant styles and markup, Added Watch.js container, Button.js component and Input.js

---
+++ src/components/Input/Input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Input = function (props) {
+    return(
+        <div>
+            <input placeholder="Input a movie" type="text"/>
+        </div>
+    )
+
+};
+
+export default Input;
---
+++ src/container/Watch.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class Watch extends Component {
+
+
+
+
+
+
+    render() {
+        return(
+            <div>
+
+            </div>
+
+        );
+    }
+}
